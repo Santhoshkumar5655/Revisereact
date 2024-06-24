b94ad9ec1e5c8dcb4ff79001099a9db16ff1b44b
unknown <chevurisanthosh@gmail.com>
2024-06-24 16:50:39 +0530

    itemlist

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -2,27 +2,22 @@ const ItemList = ({ items }) => {
   //   console.log(items);
   //   const list = items.map((s) => s.itemCards);
   //   console.log(list);
-  console.log(items);
-  const list = items.map((res) => res?.itemCards);
-  console.log(list);
+
+  const itemCards = items.map((res) => res?.itemCards);
+  console.log(itemCards);
 
   return (
     <div>
-      {list.map((h) => (
-        <span>{h?.card?.info?.name}</span>
+      {itemCards.map((item) => (
+        <div key={item?.card?.info?.id}>
+          <div>
+            <span>{item?.card?.info?.name}</span>
+            <span>{item?.card?.info?.name}</span>
+          </div>
+        </div>
       ))}
     </div>
   );
-  <div>
-    {items.map((k) => (
-      <div key={k?.card?.info?.id}>
-        <div>
-          <span>{k?.card?.info?.name}</span>
-          <span>{k?.card?.info?.name}</span>
-        </div>
-      </div>
-    ))}
-  </div>;
 };
 
 export default ItemList;
